eb915c6a8f2fccd2ed71813aa13dec9d9e142096
Vsevolod <vsevolod.lashin@gmail.com>
2023-03-31 16:27:04 +0300

    Logic of random number added

--- Randomizer/ViewController.swift
+++ Randomizer/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var minimumValueLabel: UILabel!
+    @IBOutlet var maximumValueLabel: UILabel!
+    @IBOutlet var randomValueLabel: UILabel!
+    @IBOutlet var getRandomNumberButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        getRandomNumberButton.layer.cornerRadius = 10
     }
 
-
+    @IBAction func getRandomNumber() {
+        
+        let minimumNumber = Int(minimumValueLabel.text ?? "") ?? 0
+        let maximumNumber = Int(maximumValueLabel.text ?? "") ?? 100
+        
+        randomValueLabel.text = Int.random(in: minimumNumber...maximumNumber)
+            .formatted()
+    }
 }
 
